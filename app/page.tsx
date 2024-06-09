5d5d3e979f2cc174e2aaf6b91ac43680a1b2e2a4
Jyothi327 <jyothiravi327@gmail.com>
2024-06-09 23:57:03 +0530

    commit app folder useref change 2

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Skills from './skills';
 export default function Home() {
   const [isIntersecting, setIsIntersecting] = useState(false);
   const [isBlackBg, setIsBlackBg] = useState(false);
-  const ref = useRef('');
+  const ref = useRef<HTMLDivElement | null>(null); // Explicitly type the ref
 
   const onClickBckgroundChange = () => {
     setIsBlackBg(prevState => !prevState);
@@ -35,42 +35,36 @@ export default function Home() {
 
   useEffect(() => {
     if (isIntersecting) {
-      ref.current.querySelectorAll("h2").forEach((el) => {
+      ref.current?.querySelectorAll("h2").forEach((el) => {
         el.classList.add("slide-in");
       });
-    }
-    else{
-      ref.current.querySelectorAll("h2").forEach((el) => {
+    } else {
+      ref.current?.querySelectorAll("h2").forEach((el) => {
         el.classList.remove("slide-in");
       });
-
     }
   }, [isIntersecting]);
 
   console.log('ref in home', ref);
 
   return (
-
     <div className='d-flex f-d-column c-white' id='portfolio'>
-        <div className={`pos-fixed r-0 h-3r w-3r ${isBlackBg ? 'mode-icon-black' : 'mode-icon-blue'}`}>
-            <img src={`${isBlackBg ? 
-            'https://port2bucket.s3.amazonaws.com/bulb.webp'
-            :'https://cdn-icons-png.freepik.com/512/6714/6714961.png'}`}
-            onClick={onClickBckgroundChange}
-            className={`${isBlackBg ? 'mode-black-img':''}`}/>
-        </div>
-        <Navbar onClickBckgroundChange={onClickBckgroundChange} isBlackBg={isBlackBg}/>
-        <div id='intro-div'>
-          <IntroDiv isBlackBg={isBlackBg} onClickBckgroundChange={onClickBckgroundChange} />
-        </div>
-        <div id='skills-parent-div'>
-          <Skills isBlackBg={isBlackBg}/>
-        </div>
-        
-        <div id='pro-parent-div' ref={ref}>
-          <Projects isBlackBg={isBlackBg} />
-        </div>
-        <Footer isBlackBg={isBlackBg}/> 
+      <div className={`pos-fixed r-0 h-3r w-3r ${isBlackBg ? 'mode-icon-black' : 'mode-icon-blue'}`}>
+        <img src={`${isBlackBg ? 'https://port2bucket.s3.amazonaws.com/bulb.webp' :'https://cdn-icons-png.freepik.com/512/6714/6714961.png'}`}
+             onClick={onClickBckgroundChange}
+             className={`${isBlackBg ? 'mode-black-img':''}`}/>
+      </div>
+      <Navbar onClickBckgroundChange={onClickBckgroundChange} isBlackBg={isBlackBg}/>
+      <div id='intro-div'>
+        <IntroDiv isBlackBg={isBlackBg} onClickBckgroundChange={onClickBckgroundChange} />
+      </div>
+      <div id='skills-parent-div'>
+        <Skills isBlackBg={isBlackBg}/>
+      </div>
+      <div id='pro-parent-div' ref={ref}>
+        <Projects isBlackBg={isBlackBg} />
+      </div>
+      <Footer isBlackBg={isBlackBg}/> 
     </div> 
   );
 }
